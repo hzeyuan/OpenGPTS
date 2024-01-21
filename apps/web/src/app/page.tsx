8d906414a5a2dd95b2b895cb6ba1c0d7286ae7e2
hzeyuan <599012428@qq.com>
2024-01-21 23:00:54 +0800

    opengpts v0.01

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,27 +2,13 @@
 
 import Brand from "./components/Brand";
 import GptsList from "./components/GptsList";
-import ProductHunt from "./components/ProductHunt";
+import Gradient from "./components/Gradient";
+// import ProductHunt from "./components/ProductHunt";
 import Search from "./components/Search";
 import Tab from "./components/Tab";
 import { useEffect, useState } from "react";
 
-function Gradient({
-  conic,
-  className,
-  small,
-}: {
-  small?: boolean;
-  conic?: boolean;
-  className?: string;
-}): JSX.Element {
-  return (
-    <span
-      className={`absolute mix-blend-normal will-change-[filter] rounded-[100%] ${small ? "blur-[32px]" : "blur-[75px]"
-        } ${conic ? "bg-glow-conic" : ""} ${className}`}
-    />
-  );
-}
+
 
 
 export default function Page(): JSX.Element {
@@ -64,17 +50,9 @@ export default function Page(): JSX.Element {
   return (
     <>
       <Brand count={gptsCount} />
-      <ProductHunt />
+      {/* <ProductHunt /> */}
       <Search setGpts={setGpts} setLoading={setLoading} />
       <Tab tabValue={tabValue} setTabValue={setTabValue} />
-      <div className="absolute flex  place-items-center">
-        <div className="font-sans w-auto pb-16 pt-[48px] md:pb-24 lg:pb-32 md:pt-16 lg:pt-20 flex justify-between gap-8 items-center flex-col relative z-0">
-          <Gradient
-            className="top-[-500px] opacity-[0.15] w-[1000px] h-[1000px]"
-            conic
-          />
-        </div>
-      </div>
       <GptsList gpts={gpts} loading={loading} />
     </>
   );
