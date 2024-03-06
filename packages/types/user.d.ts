70f99671cd8271673a3705dab412866d75f832d9
hdheart <7953523+hdheart@user.noreply.gitee.com>
2024-03-06 15:02:27 +0800

    add type file

---
+++ packages/types/user.d.ts
@@ -0,0 +1,10 @@
+interface UserAbilities {
+  subscription_status: string;
+  product_name: string;
+  variant_name: string;
+  product_id: number;
+  variant_id: number;
+  subscription_id: number;
+}
+
+export type { UserAbilities };
